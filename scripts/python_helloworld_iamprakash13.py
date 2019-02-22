af5d52aae7711f22865b95eb5bf86dec4aa4539c
prakash <ptan022@gmail.com>
2019-02-22 19:14:45 +0530

    added python_helloworld_iamprakash13.py

---
+++ scripts/python_helloworld_iamprakash13.py
@@ -0,0 +1,12 @@
+# LANGUAGE: Python
+# AUTHOR: Prakash 
+# GITHUB: https://github.com/iamprakash13
+
+import time
+word = "Hello World!"
+char = list(word)
+
+i = len(word)
+for i in range(0, i):
+        print(char[i])
+        time.sleep(1)
